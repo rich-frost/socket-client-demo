54ab3b645fb7ccdfe89fecac982a43425bb1ae97
Rich Frost <richard@ipponsolutions.co.uk>
2016-01-30 12:12:43 +0000

    Added ability to send message to particular car, and choose which to listen to in the client

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@
 var socket = io();
 
 socket.on('message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    var myCar = $('#myCar').val();
+
+    if (msg.car === myCar || myCar === 'All') {
+        // FIXME inline styles, just for demo!
+        $('#messages').append($('<li style="color:' + msg.car + '">').text(msg.action));
+    }
 });
 
 function emitMessage(message) {
-    socket.emit('message', message);
+    var msg = {
+        car: $('#selCar').val(),
+        action: message
+    };
+    socket.emit('message', msg);
 }
